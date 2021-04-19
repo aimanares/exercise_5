10f9d2676c448934107dbf589713a55c9a47832b
aimanares <61780861+aimanares@users.noreply.github.com>
2021-04-19 23:51:19 +0800

    Create nav.blade.php

---
+++ nav.blade.php
@@ -0,0 +1,12 @@
+<div class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a href="/about" class="nav-link">About</a>
+            </li>
+            <li class="nav-item active">
+                <a href="/list" class="nav-link">Book List</a>
+            </li>
+        </ul>
+    </div>
+</div>
