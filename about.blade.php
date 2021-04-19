b1242614c796efbcc7d3ede0dadbc87a8849b4a4
aimanares <61780861+aimanares@users.noreply.github.com>
2021-04-19 23:51:57 +0800

    Create about.blade.php

---
+++ about.blade.php
@@ -0,0 +1,7 @@
+@extends('subview.master') 
+@section('title', 'About Us') 
+@section('content') 
+ <h1>About</h1>
+ <p>This is an e-book sharing application. Feel free to browse the list of books in the <strong>'Book List'</strong></p>
+
+@endsection
